1729d48be03a2c3c38bed018c046bcb5489caa0b
Angela Engle <angelaengle67@gmail.com>
2023-09-26 13:07:12 -0500

    Completed through Task 3

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,5 +1,23 @@
 package org.launchcode.techjobs.oo;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
 public class JobTest {
     //TODO: Create your unit tests here
+    @Test
+    public void testSettingJobId(){
+        Job job1 = new Job();
+        Job job2 = new Job();
+        assertNotEquals(job1.getId(), job2.getId());
+    }
+    @Test
+    public void testJobConstructorSetsAllFields(){
+        Job testJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertTrue(testJob.getClass() = Class(Job));
+        assertEquals(testJob.getName(), "Product tester");
+    }
+
 }
