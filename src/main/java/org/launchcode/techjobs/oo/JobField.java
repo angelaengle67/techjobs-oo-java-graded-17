04534f7e9c7118211db65f64fe5a42666768f349
Angela Engle <angelaengle67@gmail.com>
2023-10-03 12:27:14 -0500

    Completed through Task 6

--- src/main/java/org/launchcode/techjobs/oo/JobField.java
+++ src/main/java/org/launchcode/techjobs/oo/JobField.java
@@ -1,4 +1,34 @@
 package org.launchcode.techjobs.oo;
 
-public class JobField {
-}
+abstract public class JobField {
+    private int id;
+    private static int nextId = 1;
+    private String value;
+
+    public JobField(){
+        this.id = nextId;
+        nextId++;
+    }
+    public JobField(String value) {
+            this();
+            this.value = value;
+        }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+    }
+
